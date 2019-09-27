624b397a23f727b0c11bb6011427dfc4cdb3afb9
Abdullah Khan <abdullah.khan3@ucalgary.ca>
2019-09-26 20:08:27 -0600

    complete assign1a

--- assignment1/assign1a.s
+++ assignment1/assign1a.s
@@ -16,82 +16,81 @@ main:
 	mov x21, 0
 	// maximum y value
 	mov x22, -500
-
-    // loop counter (from -10 to 4)
-    mov x27, -10
+    
+    // start the first iteration of the loop
 	b test
 
 test:
 	// domain: {x | -10 <= x <= 4}
 	// check if current x is = 4
-	// if so, print final result which branches to done
-	cmp x27, 4
+	// if so, print final result which then branches to done
+	cmp x20, 4
 	b.gt printResult
     
-    // reset current y before doing calculations
+    // BEGIN LOOP BODY
+
+    // reset temporary registers before doing calculations
     mov x21, 0
+    mov x23, 0
+    mov x24, 0
+    mov x25, 0
+	mov x28, 0
+
+    // 11x
+    mov x28, 11
+    mul x25, x20, x28
+    //add x25, x25, 0
+
+    // -22x^2
+    mov x24, -22
+    mul x23, x20, x20
+    mul x24, x24, x23
+    add x24, x24, x25
+
+    // reset temporary regiters used in previous calculations
+    mov x23, 0
+    mov x28, 0
 
 	// -2x^3
 	mov x23, -2
-	mul x23, x20, x20
-	mul x23, x20, x20
-	//mul x23, x20, x20
-	
-    // -22x^2
-	mov x24, -22
-	mul x24, x20, x20
-	//mul x24, x20, x20
-	
-    // 11x
-	mov x28, 11
-	mul x25, x20, x28
+	mul x28, x20, x20
+	mul x28, x28, x20
+    mul x23, x23, x28
+    add x23, x23, x24
 
-	// add the polynomial terms together to get the current y
-	//add x21, x21, x28
-
-	mov x28, 0
+    // add constant 57 to final value of y, after all the calculatios involving x
+    add x23, x23, 57
     
-    // -2x^3 + (-22x^2)
-	add x21, x23, x24
-    // + 11x
-	// add constant 57 using temp register
-	add x21, x28, x25
+    // make current max as global max if it is greater than it
+    cmp x23, x22
+    b.gt newMaximum
+    b endLoop
 
-	// reset temp register
-	mov x28, 57
-
-    // increment the loop counter
-    add x27, x27, 1
-	
+endLoop: 
     // print current x,y and max so far
     adrp x0, outputXY
     add x0, x0, :lo12:outputXY
     mov x1, x20
-	mov x2, x21
+    mov x2, x23
     bl printf
     add x20, x20, 1
-    b printMax
 
-	cmp x21, x22
-	b.gt newMaximum
-
-printMax:
 	// printf for current maximum value
 	adrp x0, outputMax
     add x0, x0, :lo12:outputMax
-    mov x1, x26
+    mov x1, x22
     bl printf
     b test
 
 newMaximum:
-    // make current max as global max
-	mov x26, x22 
+    mov x22, x23
+    b endLoop
 
 printResult:
     adrp x0, result
     add x0, x0, :lo12:result
-    mov x1, x28
-    mov x2, x26
+    mov x1, x22
+    //mov x2, x26
     bl printf
     b done
 
