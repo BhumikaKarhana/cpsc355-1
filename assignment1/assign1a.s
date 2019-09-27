3e57fa2c01a469e9a7e75a9af4700f13d219e67a
Abdullah Khan <abdullah.khan3@ucalgary.ca>
2019-09-27 12:27:15 -0600

    refactor assign1a.s

--- assignment1/assign1a.s
+++ assignment1/assign1a.s
@@ -9,15 +9,15 @@ result: .string "\nthe global max is %d\n"
 .global main
 
 main:
-    // save state
+    //save state
 	stp x29, x30, [sp, -16]!
 	mov x29, sp
 
-	// x20: x value starting from -10
+	// x20: x value starting from -10 (it acts as a loop counter and used for calcaulations
 	mov x20, -10
-	// x21: y value
+	// x21: current y value, starts at 0 and resets on every iteration
 	mov x21, 0
-	// maximum y value
+	// current maximum y value, start at a small value and it gets updated on the first iteration
 	mov x22, -500
     
     // start the first iteration of the loop
@@ -41,12 +41,14 @@ test:
 
     // 11x
     mov x28, 11
+    // use temp register to calculate 11x and store it in another temporary (used in later calculations)
     mul x25, x20, x28
-    //add x25, x25, 0
 
     // -22x^2
     mov x24, -22
+    // use temporary value to store x^2
     mul x23, x20, x20
+    // multiply and add the temporary variables to get the x term value for this polynomial term
     mul x24, x24, x23
     add x24, x24, x25
 
@@ -56,17 +58,23 @@ test:
 
 	// -2x^3
 	mov x23, -2
+    // store x^2 in temporary register 
 	mul x28, x20, x20
+    // combine all the x terms to get the final y value for the current x
 	mul x28, x28, x20
     mul x23, x23, x28
+    // add the previous x terms together
     add x23, x23, x24
 
     // add constant 57 to final value of y, after all the calculatios involving x
     add x23, x23, 57
+
+    // x23 is the current y value for the corresponding x value over here
     
     // make current max as global max if it is greater than it
     cmp x23, x22
     b.gt newMaximum
+    // branch to end of loop to execute the printf instructions
     b endLoop
 
 endLoop: 
@@ -84,6 +92,7 @@ endLoop:
 	// print current maximum value so far
 	adrp x0, outputMax
     add x0, x0, :lo12:outputMax
+    // string literal %d is the current max 
     mov x1, x22
     bl printf
     // move to the next iteration of the loop after printing to stdout
@@ -98,6 +107,7 @@ printResult:
     // printing final result, the maximum value of the polynomial
     adrp x0, result
     add x0, x0, :lo12:result
+    // value for string literal %d is current_max register
     mov x1, x22
     bl printf
     b done
