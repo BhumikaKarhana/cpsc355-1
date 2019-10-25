89d778c7d95ef1470bdec9d8e6ac84e17a617d11
Abdullah Khan <abdullah.khan3@ucalgary.ca>
2019-10-24 22:55:23 -0600

    complete assign3.asm
    it works!

--- assignment3/assign3.asm
+++ assignment3/assign3.asm
@@ -11,7 +11,7 @@ element:    .string "V[%d]: %d\n"                   // print array elements (sam
 i_size  =  4                                        // size of each array element
 size    =  50                                       // 50 elements in array
 v_size  =  size * i_size                            // total size of all array elements
-alloc   =  -(16 + 16 + v_size) & -16                // pre-increment value for the stack pointer (used in main "prologue")
+alloc   =  -(16 + 16 + size) & -16                  // pre-increment value for the stack pointer (used in main "prologue")
 dealloc =  -alloc                                   // post-increment value for the stack pointer (used in main "epilogue")
 
 // assembler equates for offsets for i, j, min, and the array base
@@ -34,8 +34,9 @@ main:
     mov     fp, sp                                  // update frame pointer to the current stack pointer
 
     mov     base, fp                                // set the base address for array
-    add     base, base, i_s                         // array base location calculated by adding its offset to fp
+    add     base, base, ia_s                        // array base location calculated by adding its offset to fp
 
+    ldr     index, [fp, i_s]                        // load current index from stack
     mov     index, 0                                // initialize index variable to 0 in the beginning
     str     index, [fp, i_s]                        // store index on stack
 
@@ -44,18 +45,18 @@ main:
 define(randomNum, w22)                              // macro used to perform ops on the result of the rand function
 
 loop1:                                              // first for-loop (to generate random array)
-    bl      rand                                    // call library function rand()
     ldr     index, [fp, i_s]                        // load current index from stack
+    bl      rand                                    // call library function rand()
     and     randomNum, w0, 0xFF                     // and operation on random number stored in w0 and 0xFF (255)
     str     randomNum, [base, index, SXTW 2]        // store result in array at current index
 
 
-    ldr    randomNum, [base, index, SXTW 2]              //  
+    ldr    randomNum, [base, index, SXTW 2]         // load random number from stack (to print below) 
     // print string literal to standard output 
     adrp    x0, element                             
     add     x0, x0, :lo12:element                   
     mov     w1, index                               // first argument of printf is the index
-    mov     w2, randomNum                                // second argument of printf is the random number itself
+    mov     w2, randomNum                           // second argument of printf is the random number itself
     bl      printf                                  // branch and link printf
     
     mov     temp, 0
@@ -82,7 +83,7 @@ loop2:                                              // second for-loop (for sele
     mov     min, index                              // set min = i
     str     min, [fp, min_s]                        // store min on stack
 
-    ldr     j_index, [fp, j_s]                        // load current index from stack
+    ldr     j_index, [fp, j_s]                      // load current index from stack
     ldr     index, [fp, i_s]                        // load current index from stack
     mov     j_index, index                          // set j = i
     add     j_index, j_index, 1                     // increment j (for loop condition)
@@ -94,58 +95,49 @@ define(vMin, w23)                                   // register to store V[min] 
 define(vJ, w24)                                     // register to store V[j] (used for comparison in selection sort)
 
 loop3:                                              // inner for-loop in loop2
-//    mov     vMin, 0
-//    mov     vJ, 0
     ldr     min, [fp, min_s]                        // load min from stack
-    ldr     j_index, [fp, j_s]                        // load current index from stack
+    ldr     j_index, [fp, j_s]                      // load current index from stack
 
-    //ldr     vJ, [base, j_index, SXTW 2]
-    //ldr     vMin, [base, min, SXTW 2]
-    ldr     vJ,[base, j_index, SXTW 2]              // load V[j] from array on stack
     ldr     vMin, [base, min, SXTW 2]               // load V[min] from array on stack
+    ldr     vJ,[base, j_index, SXTW 2]              // load V[j] from array on stack
 
     cmp     vMin, vJ                                // compare V[j] to V[min]
     b.lt    continue                                // skip to continue
 
-    mov     min, j_index
-    str     j_index, [fp, min_s]                    // store j on stack in min's position (min = j)
+    mov     min, j_index                            // min = j
+    str     j_index, [fp, min_s]                    // store j on stack in min's position
 
 continue:
-    //add     index, index, 1
-    //str     index, [fp, i_s]                        // store new index on stack
-    //mov     min, j_index
-    // ldr     j_index, [fp, j_s]                        // load current index from stack
+    ldr     j_index, [fp, j_s]                      // load current index from stack
     add     j_index, j_index, 1
     str     j_index, [fp, j_s]                      // store j on stack
 
 loop3Test:                                          // inner for-loop test
-    //ldr     j_index, [fp, j_s]                      // load current index from stack
+    ldr     j_index, [fp, j_s]                      // load current index from stack
     cmp     j_index, size                           // compare j to array size
     b.lt    loop3                                   // check if index < 50 and if so, iterate again
 
     ldr     min, [fp, min_s]                        // load min from stack
+    ldr     vMin, [base, min, SXTW 2]               // load V[j] from array on stack
     ldr     vJ, [base, j_index, SXTW 2]             // load V[j] from array on stack
-    ldr     vMin, [base, min, SXTW 2]             // load V[j] from array on stack
 
     ldr     index, [fp, i_s]                        // load current index from stack
-    ldr     w27, [base, index ,SXTW 2]               // v[i]
-    //mov     temp, vMin
-    //ldr     w25, [base, index, SXTW 2]
+    ldr     w27, [base, index ,SXTW 2]              // store v[i] in w27
+    
+    // swap elements
+    mov     temp, w27                               // temp = v[min] 
+    mov     w27, vMin                               // v[min] = v[i]
+    mov     vMin, temp                              // v[i] = temp
 
-    mov     temp, w27
-    mov     w27, vMin
-    mov     vMin, temp
-    //mov     vMin, w25
-    //mov     w25, temp
-
-    str     vMin, [base, min, SXTW 2]
-    str     w27, [base, index, SXTW 2]
+    str     vMin, [base, min, SXTW 2]               // store current min in vMin
+    str     w27, [base, index, SXTW 2]              // store v[i] in w27
 
     // print string literal to standard output 
     adrp    x0, element
     add     x0, x0, :lo12:element
-    mov     w0, index
-    mov     w1, w27
+    mov     w1, index                               // first argument for printf is the current index
+    ldr     w27, [base, index ,SXTW 2]              // load v[i] using current index
+    mov     w2, w27                                 // second argument fro printf is v[i]
     bl      printf                                  // branch and link printf
     
     ldr     index, [fp, i_s]                        // load current index from stack
@@ -161,7 +153,3 @@ done:
    ldp      fp, lr, [sp], dealloc                   // deallocate stack memory
    ret                                              // return with exit code
 
-
-
-
-   //x/50wd $address it'll display 50 word sized integers from memory
